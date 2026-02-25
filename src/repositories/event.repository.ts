6e49c8d575b4b19680d4e160a08410a1d0423ed7
FlazeFy2 <flazen.edu@gmail.com>
2026-02-25 11:07:13 +0700

    feat(event) : show upcoming event for event organizer in GET upcoming event API

--- src/repositories/event.repository.ts
+++ src/repositories/event.repository.ts
@@ -32,36 +32,82 @@ export class EventRepository {
     }
 
     public findUpcomingEventRepo = async (userId: string, role: string) => {
-        return await prisma.event_schedule.findMany({
-            where: {
-                event: {
-                    transactions: {
-                        some: { customer_id: userId }
-                    }
-                }
-            },
-            orderBy: { start_date: 'desc' },
-            take: 3,
-            select: {
-                start_date: true, end_date: true,
-                venue: {
-                    select: {
-                        venue_name: true, venue_address: true, venue_coordinate: true,
-                    },
-                },
-                event: {
-                    select: {
-                        id: true, event_title: true, event_category: true,
+        if (role === "customer") {
+            return await prisma.event_schedule.findMany({
+                where: {
+                    event: {
                         transactions: {
-                            where: { customer_id: userId },
-                            select: {
-                                attendees: { select: { fullname: true } }
+                            some: { customer_id: userId }
+                        }
+                    }
+                },
+                orderBy: { start_date: 'desc' },
+                take: 3,
+                select: {
+                    start_date: true, end_date: true,
+                    venue: {
+                        select: {
+                            venue_name: true, venue_address: true, venue_coordinate: true,
+                        },
+                    },
+                    event: {
+                        select: {
+                            id: true, event_title: true, event_category: true,
+                            transactions: {
+                                where: { customer_id: userId },
+                                select: {
+                                    attendees: { select: { fullname: true } }
+                                }
                             }
                         }
                     }
                 }
-            }
-        })
+            })
+        } else {
+            return await prisma.event_schedule.findMany({
+                where: {
+                    event: { event_organizer_id: userId }
+                },
+                orderBy: { start_date: 'desc' },
+                take: 3,
+                select: {
+                    start_date: true, end_date: true,
+                    venue: {
+                        select: {
+                            venue_name: true, venue_address: true, venue_coordinate: true,
+                        },
+                    },
+                    event: {
+                        select: {
+                            id: true, event_title: true, event_category: true, maximum_seat: true,
+                            transactions: {
+                                select: {
+                                    amount: true, paid_off_at: true, attendees: { select: { id: true } }
+                                }
+                            }
+                        }
+                    }
+                }
+                }).then(events => {
+                    // remaining calculaiton to find total profit and seat remaining
+                    return events.map(e => {
+                        const totalProfit = e.event.transactions.filter(dt => dt.paid_off_at !== null).reduce((sum, idx) => sum + idx.amount, 0)
+                        const totalAttendees = e.event.transactions.reduce((sum, idx) => sum + idx.attendees.length, 0)
+                
+                        return {
+                            ...e, event: {
+                                id: e.event.id,
+                                event_title: e.event.event_title,
+                                event_category: e.event.event_category,
+                                maximum_seat: e.event.maximum_seat,
+                                total_profit: totalProfit,
+                                total_seat_remaining: e.event.maximum_seat - totalAttendees
+                            }
+                        }
+                    }
+                )
+            })
+        }
     }
 
     public deleteEventByIdRepo = async (userId: string, eventId: string) => {
