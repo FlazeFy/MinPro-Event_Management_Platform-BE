466e462fe8d194159d50ad39688691d69f4eabe9
FlazeFy2 <flazen.edu@gmail.com>
2026-02-27 10:42:40 +0700

    feat(seeder & migration) : add customer point seeder & migration
    replace previous customer's point business logic

--- src/const.ts
+++ src/const.ts
@@ -1,2 +1,2 @@
-export const extraPointForOwner: number = 20000
-export const extraPointForUser: number = 10000
+export const extraPointForOwner: number = 10000
+export const extraDiscountForUser: number = 10
